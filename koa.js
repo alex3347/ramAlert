b7a73eb8cc0031fdc4aae1f1640cb99cdd4defe3
alex <821882886@qq.com>
2018-07-09 16:04:08 +0800

    server completed

--- koa.js
+++ koa.js
@@ -1,34 +1,61 @@
 const Koa = require('koa');
-const app = new Koa();
 
 const Router = require('koa-router');
+//用于render
+const views = require('koa-views');
+//用于模版引入静态文件
+const serve = require('koa-static');
+//用于引入文件夹路径
+const path = require('./path.js');
+const Monk = require('monk');
+
+
+const app = new Koa();
 const router = new Router();
 
-const views = require('koa-views');
-const path = require('./path.js');
+const db=new Monk('localhost/userName');//链接到库
+const test1 = db.get('test1');//表
 
-router.use(views(path + '/ram'));
+app.use(serve(path));
+//配置模板引擎
+router.use(views(path + '/lib/'));
 
-app.use(router.routes())    //路由都引入进来
+app.use(router.routes())
     .use(router.allowedMethods());
 
-const about = ctx => {
-    ctx.response.type = 'html';
-    ctx.response.body = '<a href="/">Index Page</a>';
+const addMem = async (ctx, next) => {
+        await ctx.render('addMem.html');
 };
 
-const main = ctx => {
-    ctx.response.body = 'Hello World';
+const addData = async (ctx, next) => {
+    let token = parseInt(Math.random()*100000+1)+ '';
+    let key = ctx.params.key+''+token;
+
+    let st = await test1.find({name:key});
+
+    if(st.length !== 0){
+        ctx.response.body = '用户名已存在，请检查输入是否正确';
+    }else{
+        await test1.insert({ name:key});
+        ctx.response.body = 'http://localhost:3000/ram/'+key;
+    }
 };
 
-const test = async (ctx, next) => {
-    await ctx.render('index.html');
+const ram = async (ctx, next) => {
+    let key = ctx.params.key+'';
+    let st = await test1.find({name:key});
+
+    if(st.length !== 0){
+        await ctx.render('index.html');
+    }else{
+        ctx.response.body = '<p style="margin:0 auto;width:400px;">支付5Eos至 <span style="color:green;font-size:18px;">占位</span> <br>填写Memo:您的邮箱 <span style="color:gray">(例：123456@qq.com)</span></p><p style="margin:0 auto;width:400px;">之后五分钟内您将收到Ram价格预警链接</p><p style="margin:0 auto;width:400px;">如果忘记填写Memo,请发送邮件至</p>';
+    }
 };
 
 
-router.get('/', main);
-router.get('/about', about);
-router.get('/test', test);
+router.get('/addMem', addMem);
+router.get('/addData/:key?', addData);
+router.get('/ram/:key?', ram);
 
 
 app.listen(3000, function () {
--- koaStatic.js
+++
@@ -1,5 +0,0 @@
-const Koa = require('koa');
-const app = new Koa();
-const static = require("koa-static");
-app.use(static(__dirname+ "/ram",{ extensions: ['html']}));
-app.listen(3000);
